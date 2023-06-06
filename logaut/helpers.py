bf8e28eaa3a6717934bb09f4091ff58a693569b0
MarcoFavorito <marco.favorito@gmail.com>
2023-06-06 09:53:30 +0200

    fix: lydia-backend, use temporary directory and save formula on filesystem

--- logaut/helpers.py
+++ logaut/helpers.py
@@ -21,8 +21,11 @@
 #
 
 """Helpers module."""
-
+import contextlib
 import re
+import tempfile
+from pathlib import Path
+from typing import Generator
 
 
 class RegexConstrainedString(str):
@@ -55,3 +58,24 @@ class RegexConstrainedString(str):
                 data=self, regex=self.REGEX
             )
         )
+
+
+@contextlib.contextmanager
+def temporary_directory() -> Generator[Path, None, None]:
+    """
+    Create a temporary directory and clean up when done.
+
+    This function is a context manager that creates a temporary directory.
+    It wraps tempfile.TemporaryDirectory in order to make the clean up more robust
+    (e.g. managing a PermissionError in Windows: https://www.scivision.dev/python-tempfile-permission-error-windows/).
+    """
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_path = Path(temp_dir.name)
+
+    yield temp_path
+
+    # when done with temporary directory
+    try:
+        temp_dir.cleanup()
+    except PermissionError:
+        pass
